618b6a7fbbb286f4faddf1bfceec9d3cbf77b209
ozbayb <17261091+ozbayb@users.noreply.github.com>
2024-03-14 11:20:33 -0600

    Update install.bat and prompt.bat scripts

--- install.bat
+++ install.bat
@@ -1,5 +1,2 @@
 @echo off
 Powershell.exe -executionpolicy bypass -NoExit -Command ". devwraps\scripts\base.ps1; . scripts\install.ps1;"
-
-
-
--- prompt.bat
+++ prompt.bat
@@ -1,2 +1,12 @@
 @echo off
-Powershell.exe -executionpolicy bypass -NoExit -Command ". devwraps\scripts\base.ps1; Activate-Anaconda"
+
+FOR /F "tokens=1,2 delims==" %%i IN (.\config.txt) DO (
+    IF "%%i"=="ANACONDA_ENV_NAME" SET ENVIRONMENT_NAME=%%j
+)
+
+IF "%ENVIRONMENT_NAME%"=="" (
+    echo Error: Anaconda environment name is not set.
+    exit /b 1
+)
+
+Powershell.exe -executionpolicy bypass -NoExit -Command ". devwraps\scripts\base.ps1; Activate-Anaconda -environmentName %ENVIRONMENT_NAME%"
